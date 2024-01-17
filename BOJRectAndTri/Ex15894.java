d73d63bdcd2ae9ad87c57e3cf46f022e076f9ffd
sanghun-kim-06 <136154722+sanghun-kim-06@users.noreply.github.com>
2024-01-17 12:43:58 +0900

    Create Ex15894.java

---
+++ BOJRectAndTri/Ex15894.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex15894 {
+
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		long n = scan.nextLong();
+		System.out.println(n*4);
+	}
+
+}
